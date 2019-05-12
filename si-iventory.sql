d7add1830f22fb8823f1306a5285fb0099fe9e39
aidiaoemami <aidiaoemami@gmail.com>
2019-05-12 20:23:39 +0700

    tambah sql

---
+++ si-iventory.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2019 at 04:51 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `si-iventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang_keluar`
+--
+
+CREATE TABLE `tb_barang_keluar` (
+  `id` int(10) NOT NULL,
+  `id_transaksi` varchar(50) NOT NULL,
+  `tanggal_masuk` varchar(20) NOT NULL,
+  `tanggal_keluar` varchar(20) NOT NULL,
+  `lokasi` varchar(100) NOT NULL,
+  `kode_barang` varchar(100) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  `jumlah` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_barang_keluar`
+--
+
+INSERT INTO `tb_barang_keluar` (`id`, `id_transaksi`, `tanggal_masuk`, `tanggal_keluar`, `lokasi`, `kode_barang`, `nama_barang`, `satuan`, `jumlah`) VALUES
+(18, 'SI-201997865124', '12/05/2019', '22/05/2019', 'Jawa Barat', 'A21', 'Kereta', 'Dus', '1');
+
+--
+-- Triggers `tb_barang_keluar`
+--
+DELIMITER $$
+CREATE TRIGGER `TG_BARANG_KELUAR` AFTER INSERT ON `tb_barang_keluar` FOR EACH ROW BEGIN
+ UPDATE tb_barang_masuk SET jumlah=jumlah-NEW.jumlah
+ WHERE kode_barang=NEW.kode_barang;
+ DELETE FROM tb_barang_masuk WHERE jumlah = 0;
+
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang_masuk`
+--
+
+CREATE TABLE `tb_barang_masuk` (
+  `id_transaksi` varchar(50) NOT NULL,
+  `tanggal` varchar(20) NOT NULL,
+  `lokasi` varchar(100) NOT NULL,
+  `kode_barang` varchar(100) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  `jumlah` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_barang_masuk`
+--
+
+INSERT INTO `tb_barang_masuk` (`id_transaksi`, `tanggal`, `lokasi`, `kode_barang`, `nama_barang`, `satuan`, `jumlah`) VALUES
+('SI-201965972043', '12/05/2019', 'Jawa Barat', 'B22', 'Besi', 'Dus', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_satuan`
+--
+
+CREATE TABLE `tb_satuan` (
+  `id_satuan` int(11) NOT NULL,
+  `kode_satuan` varchar(100) NOT NULL,
+  `nama_satuan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_satuan`
+--
+
+INSERT INTO `tb_satuan` (`id_satuan`, `kode_satuan`, `nama_satuan`) VALUES
+(1, 'Dus', 'Dus'),
+(2, 'Pcs', 'Pcs'),
+(5, 'Pack', 'Pack');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_upload_gambar_user`
+--
+
+CREATE TABLE `tb_upload_gambar_user` (
+  `id` int(11) NOT NULL,
+  `username_user` varchar(100) NOT NULL,
+  `nama_file` varchar(220) NOT NULL,
+  `ukuran_file` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_upload_gambar_user`
+--
+
+INSERT INTO `tb_upload_gambar_user` (`id`, `username_user`, `nama_file`, `ukuran_file`) VALUES
+(1, 'zahidin', 'nopic5.png', '6.33'),
+(2, 'test', 'nopic4.png', '6.33'),
+(3, 'coba', 't23.jpg', '56.7'),
+(4, 'admin', 'avatar04.png', '13.22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(12) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role` tinyint(4) NOT NULL DEFAULT '0',
+  `last_login` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `role`, `last_login`) VALUES
+(16, 'test', 'test@gmail.com', '$2y$10$CTjzvmT5B.dxojKZOxsjTeMc4E7.Gwl9slAgX.0lozwGrKSMxzWdO', 1, '16-03-2018 4:46'),
+(17, 'coba', 'coba@gmail.com', '$2y$10$WRMyjAi8nnkr3J3QvzvyHuEoqay5dYd5NgMJKxsxtXKCp8.JCxZm.', 1, '11-05-2019 22:42'),
+(20, 'admin', 'admin@gmail.com', '$2y$10$3HNkMOtwX8X88Xb3DIveYuhXScTnJ9m16/rPDF1/VTa/VTisxVZ4i', 1, '12-05-2019 2:48'),
+(21, 'bannipebriansyah', 'bannipebriansyah@gmail.com', '$2y$10$jQiZ/Ml4kApJQ4KM7rp65.yVXccJoV.QeuJMWu/KWGLbNCv0yiodS', 0, '10-05-2019 2:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_barang_keluar`
+--
+ALTER TABLE `tb_barang_keluar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_barang_masuk`
+--
+ALTER TABLE `tb_barang_masuk`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `tb_satuan`
+--
+ALTER TABLE `tb_satuan`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indexes for table `tb_upload_gambar_user`
+--
+ALTER TABLE `tb_upload_gambar_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_barang_keluar`
+--
+ALTER TABLE `tb_barang_keluar`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `tb_satuan`
+--
+ALTER TABLE `tb_satuan`
+  MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tb_upload_gambar_user`
+--
+ALTER TABLE `tb_upload_gambar_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
