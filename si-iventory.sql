576c1f81c6b2cdd2a9a1ea3b153cb93259f2e273
aidiaoemami <aidiaoemami@gmail.com>
2019-05-15 09:31:46 +0700

    tambah tabel jenis barang

--- si-iventory.sql
+++ si-iventory.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2019 at 04:51 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- Waktu pembuatan: 15 Bulan Mei 2019 pada 04.30
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.2.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,14 +25,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_barang_keluar`
+-- Struktur dari tabel `tb_barang_keluar`
 --
 
 CREATE TABLE `tb_barang_keluar` (
   `id` int(10) NOT NULL,
   `id_transaksi` varchar(50) NOT NULL,
-  `tanggal_masuk` varchar(20) NOT NULL,
-  `tanggal_keluar` varchar(20) NOT NULL,
+  `tanggal_masuk` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `tanggal_keluar` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `lokasi` varchar(100) NOT NULL,
   `kode_barang` varchar(100) NOT NULL,
   `nama_barang` varchar(100) NOT NULL,
@@ -41,14 +41,14 @@ CREATE TABLE `tb_barang_keluar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_barang_keluar`
+-- Dumping data untuk tabel `tb_barang_keluar`
 --
 
 INSERT INTO `tb_barang_keluar` (`id`, `id_transaksi`, `tanggal_masuk`, `tanggal_keluar`, `lokasi`, `kode_barang`, `nama_barang`, `satuan`, `jumlah`) VALUES
-(18, 'SI-201997865124', '12/05/2019', '22/05/2019', 'Jawa Barat', 'A21', 'Kereta', 'Dus', '1');
+(23, 'SI-201942618039', '2019-05-13 12:10:13', '2019-05-13 05:11:35', 'Jawa Barat', 'B23', 'Panel Surya', 'Pcs', '1');
 
 --
--- Triggers `tb_barang_keluar`
+-- Trigger `tb_barang_keluar`
 --
 DELIMITER $$
 CREATE TRIGGER `TG_BARANG_KELUAR` AFTER INSERT ON `tb_barang_keluar` FOR EACH ROW BEGIN
@@ -63,12 +63,12 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_barang_masuk`
+-- Struktur dari tabel `tb_barang_masuk`
 --
 
 CREATE TABLE `tb_barang_masuk` (
   `id_transaksi` varchar(50) NOT NULL,
-  `tanggal` varchar(20) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `lokasi` varchar(100) NOT NULL,
   `kode_barang` varchar(100) NOT NULL,
   `nama_barang` varchar(100) NOT NULL,
@@ -77,16 +77,35 @@ CREATE TABLE `tb_barang_masuk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_barang_masuk`
+-- Dumping data untuk tabel `tb_barang_masuk`
 --
 
 INSERT INTO `tb_barang_masuk` (`id_transaksi`, `tanggal`, `lokasi`, `kode_barang`, `nama_barang`, `satuan`, `jumlah`) VALUES
-('SI-201965972043', '12/05/2019', 'Jawa Barat', 'B22', 'Besi', 'Dus', '1');
+('SI-201942618039', '2019-05-13 05:11:35', 'Jawa Barat', 'B23', 'Panel Surya', 'Pcs', '1');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_satuan`
+-- Struktur dari tabel `tb_jenisbarang`
+--
+
+CREATE TABLE `tb_jenisbarang` (
+  `id_jenisbarang` int(11) NOT NULL,
+  `kode_jenisbarang` varchar(20) NOT NULL,
+  `nama_jenisbarang` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_jenisbarang`
+--
+
+INSERT INTO `tb_jenisbarang` (`id_jenisbarang`, `kode_jenisbarang`, `nama_jenisbarang`) VALUES
+(1, 'A22', 'Besi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_satuan`
 --
 
 CREATE TABLE `tb_satuan` (
@@ -96,7 +115,7 @@ CREATE TABLE `tb_satuan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_satuan`
+-- Dumping data untuk tabel `tb_satuan`
 --
 
 INSERT INTO `tb_satuan` (`id_satuan`, `kode_satuan`, `nama_satuan`) VALUES
@@ -107,7 +126,7 @@ INSERT INTO `tb_satuan` (`id_satuan`, `kode_satuan`, `nama_satuan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_upload_gambar_user`
+-- Struktur dari tabel `tb_upload_gambar_user`
 --
 
 CREATE TABLE `tb_upload_gambar_user` (
@@ -118,19 +137,20 @@ CREATE TABLE `tb_upload_gambar_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_upload_gambar_user`
+-- Dumping data untuk tabel `tb_upload_gambar_user`
 --
 
 INSERT INTO `tb_upload_gambar_user` (`id`, `username_user`, `nama_file`, `ukuran_file`) VALUES
 (1, 'zahidin', 'nopic5.png', '6.33'),
 (2, 'test', 'nopic4.png', '6.33'),
 (3, 'coba', 't23.jpg', '56.7'),
-(4, 'admin', 'avatar04.png', '13.22');
+(4, 'admin', 'avatar04.png', '13.22'),
+(5, 'aidiaoemami', 'nopic2.png', '6.33');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -143,76 +163,89 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `role`, `last_login`) VALUES
 (16, 'test', 'test@gmail.com', '$2y$10$CTjzvmT5B.dxojKZOxsjTeMc4E7.Gwl9slAgX.0lozwGrKSMxzWdO', 1, '16-03-2018 4:46'),
 (17, 'coba', 'coba@gmail.com', '$2y$10$WRMyjAi8nnkr3J3QvzvyHuEoqay5dYd5NgMJKxsxtXKCp8.JCxZm.', 1, '11-05-2019 22:42'),
-(20, 'admin', 'admin@gmail.com', '$2y$10$3HNkMOtwX8X88Xb3DIveYuhXScTnJ9m16/rPDF1/VTa/VTisxVZ4i', 1, '12-05-2019 2:48'),
-(21, 'bannipebriansyah', 'bannipebriansyah@gmail.com', '$2y$10$jQiZ/Ml4kApJQ4KM7rp65.yVXccJoV.QeuJMWu/KWGLbNCv0yiodS', 0, '10-05-2019 2:04');
+(20, 'admin', 'admin@gmail.com', '$2y$10$3HNkMOtwX8X88Xb3DIveYuhXScTnJ9m16/rPDF1/VTa/VTisxVZ4i', 1, '14-05-2019 10:41'),
+(21, 'bannipebriansyah', 'bannipebriansyah@gmail.com', '$2y$10$jQiZ/Ml4kApJQ4KM7rp65.yVXccJoV.QeuJMWu/KWGLbNCv0yiodS', 0, '10-05-2019 2:04'),
+(25, 'aidiaoemami', 'aidiaoemami@gmail.com', '$2y$10$Uca3sAtZd3cHazvFz.foDek8biVvcI3vJyX3tfuoC0uk7B9g0DN4G', 0, '14-05-2019 10:42');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_barang_keluar`
+-- Indeks untuk tabel `tb_barang_keluar`
 --
 ALTER TABLE `tb_barang_keluar`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_barang_masuk`
+-- Indeks untuk tabel `tb_barang_masuk`
 --
 ALTER TABLE `tb_barang_masuk`
   ADD PRIMARY KEY (`id_transaksi`);
 
 --
--- Indexes for table `tb_satuan`
+-- Indeks untuk tabel `tb_jenisbarang`
+--
+ALTER TABLE `tb_jenisbarang`
+  ADD PRIMARY KEY (`id_jenisbarang`);
+
+--
+-- Indeks untuk tabel `tb_satuan`
 --
 ALTER TABLE `tb_satuan`
   ADD PRIMARY KEY (`id_satuan`);
 
 --
--- Indexes for table `tb_upload_gambar_user`
+-- Indeks untuk tabel `tb_upload_gambar_user`
 --
 ALTER TABLE `tb_upload_gambar_user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tb_barang_keluar`
+-- AUTO_INCREMENT untuk tabel `tb_barang_keluar`
 --
 ALTER TABLE `tb_barang_keluar`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT for table `tb_satuan`
+-- AUTO_INCREMENT untuk tabel `tb_jenisbarang`
+--
+ALTER TABLE `tb_jenisbarang`
+  MODIFY `id_jenisbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_satuan`
 --
 ALTER TABLE `tb_satuan`
   MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `tb_upload_gambar_user`
+-- AUTO_INCREMENT untuk tabel `tb_upload_gambar_user`
 --
 ALTER TABLE `tb_upload_gambar_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
